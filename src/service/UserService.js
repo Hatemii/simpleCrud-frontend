216f96dca21a7278cce74906c9e532f7e0bf2dc8
Hatemii <hatem.xhezairi96@gmail.com>
2020-11-14 21:41:42 +0100

    Api Setup

---
+++ src/service/UserService.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+const Students_Rest_Api = "http://localhost:8080/students"
+
+class UserService {
+
+    getAllStudents() {
+        return axios.get(Students_Rest_Api);
+    }
+}
+
+export default UserService();
